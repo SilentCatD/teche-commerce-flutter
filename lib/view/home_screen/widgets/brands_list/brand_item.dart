497151c9340ac630b45e9dca68d41f94439da0d8
Silent Cat <ngocphuocnguyen312@gmail.com>
2022-03-05 21:03:01 +0700

    add ontap event template

--- lib/view/home_screen/widgets/brands_list/brand_item.dart
+++ lib/view/home_screen/widgets/brands_list/brand_item.dart
@@ -8,32 +8,35 @@ class BrandItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(right: 20),
-      height: 100,
-      width: 200,
-      decoration: BoxDecoration(
-        color: Colors.black,
-        borderRadius: BorderRadius.circular(20),
-        image: brand.imgUrl != null
-            ? DecorationImage(
-                opacity: 0.5,
-                image: Image.network(
-                  brand.imgUrl!,
-                ).image,
-                fit: BoxFit.fill,
-              )
-            : null,
-      ),
-      child: Align(
-        alignment: Alignment.centerLeft,
-        child: Container(
-          margin: const EdgeInsets.only(left: 20),
-          child: Text(
-            brand.name,
-            overflow: TextOverflow.ellipsis,
-            style: const TextStyle(
-                color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
+    return GestureDetector(
+      onTap: (){},
+      child: Container(
+        margin: const EdgeInsets.only(right: 20),
+        height: 100,
+        width: 200,
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(20),
+          image: brand.imgUrl != null
+              ? DecorationImage(
+                  opacity: 0.5,
+                  image: Image.network(
+                    brand.imgUrl!,
+                  ).image,
+                  fit: BoxFit.fill,
+                )
+              : null,
+        ),
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Container(
+            margin: const EdgeInsets.only(left: 20),
+            child: Text(
+              brand.name,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                  color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
+            ),
           ),
         ),
       ),
