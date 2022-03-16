ae821a0d383719bf02ce459343cbde15f84b1370
Silent Cat <ngocphuocnguyen312@gmail.com>
2022-03-16 23:13:00 +0700

    add button to item card

--- lib/view/commons/product_card/product_card.dart
+++ lib/view/commons/product_card/product_card.dart
@@ -17,77 +17,107 @@ class ProductCard extends StatelessWidget {
       width: ProductCard.cardWidth,
       child: InkWell(
         onTap: () {
-         Navigator.of(context).pushNamed(ProductScreen.pageRoute, arguments: product.id);
+          Navigator.of(context)
+              .pushNamed(ProductScreen.pageRoute, arguments: product.id);
         },
         child: Card(
           elevation: 4,
-          child: LayoutBuilder(
-            builder: (BuildContext context, BoxConstraints constraints) {
-              final maxHeight = constraints.maxHeight;
-              final maxWidth = constraints.maxWidth;
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Center(
-                    child: SizedBox(
-                      height: maxHeight * 0.7,
-                      child: product.imageUrls.isNotEmpty ?
-                           FadeInImage.memoryNetwork(
-                              placeholder: kTransparentImage,
-                              image: product.imageUrls[0],
-                              fit: BoxFit.cover,
-                            )
-                          : Container(
-                              color: Colors.grey,
-                            ),
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(10),
-                    height: maxHeight * 0.3,
-                    child: Column(
-                      children: [
-                        Text(
-                          product.name,
-                          style: const TextStyle(
-                            fontSize: 15,
-                            color: Colors.black54,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        const Spacer(),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Container(
-                              constraints:
-                                  BoxConstraints(maxWidth: maxWidth * 0.4),
-                              child: Text(
-                                "\$${product.price.toString().padRight(5, '0')}",
-                                overflow: TextOverflow.ellipsis,
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 15,
+          child: Stack(
+            children: [
+              LayoutBuilder(
+                builder: (BuildContext context, BoxConstraints constraints) {
+                  final maxHeight = constraints.maxHeight;
+                  final maxWidth = constraints.maxWidth;
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: SizedBox(
+                          height: maxHeight * 0.7,
+                          child: product.imageUrls.isNotEmpty
+                              ? FadeInImage.memoryNetwork(
+                                  placeholder: kTransparentImage,
+                                  image: product.imageUrls[0],
+                                  fit: BoxFit.cover,
+                                )
+                              : Container(
+                                  color: Colors.grey,
                                 ),
-                              ),
-                            ),
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        height: maxHeight * 0.3,
+                        child: Column(
+                          children: [
                             Text(
-                              "⭐${product.rateAverage.toString()}",
-                              overflow: TextOverflow.ellipsis,
+                              product.name,
                               style: const TextStyle(
-                                fontWeight: FontWeight.bold,
                                 fontSize: 15,
+                                color: Colors.black54,
+                                fontWeight: FontWeight.bold,
                               ),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            const Spacer(),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Container(
+                                  constraints:
+                                      BoxConstraints(maxWidth: maxWidth * 0.4),
+                                  child: Text(
+                                    "\$${product.price.toString().padRight(5, '0')}",
+                                    overflow: TextOverflow.ellipsis,
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  "⭐${product.rateAverage.toString()}",
+                                  overflow: TextOverflow.ellipsis,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15,
+                                  ),
+                                ),
+                              ],
                             ),
                           ],
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+              Positioned(
+                right: 0,
+                top: 0,
+                child: Container(
+                  color: Colors.black54,
+                  child: Column(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.favorite,
+                          color: Colors.amber,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.add_shopping_cart,
+                          color: Colors.amber,
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              );
-            },
+                ),
+              ),
+            ],
           ),
         ),
       ),
